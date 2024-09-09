4f6ae89804bfedef22fe139eec5874023639cf1a
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2024-09-09 21:52:45 +0100

    Initial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector3(24, 3, 0);
+        transform.position = plane.transform.position + offset;
+    }
+}
